ce3b26e0d19f2f04eca0fc896c7fb6c1c2586635
Faten Nuraina <fatennuraina02@gmail.com>
2026-04-13 19:25:07 +0800

    done today

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main()  { runApp
-  (MaterialApp
-  (home: Scaffold(
-  backgroundColor: Colors.yellow,
-  body: Center(
-    child: Text(
-        'FATEN NURAINA',
-      style: TextStyle (fontSize: 50.0),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+    backgroundColor: Colors.purple,
+body: SafeArea(
+  child: Container(
+    height: 250,
+    width: 250,
+    margin: EdgeInsets.symmetric(
+      vertical: 50.0,
+          horizontal: 10,
+    ),
+    color: Colors.black,
+  child: Text('FATEN NURAINA',
+  style: TextStyle(
+  color: Colors.white
   ),
+  
   ),
-  appBar:AppBar(
-    backgroundColor: Colors.blueGrey.shade900,
-    title: Center(child: Text('HELLO WORLD')),
   ),
 ),
-),
-);
+    ),
+    );
+  }
 }
