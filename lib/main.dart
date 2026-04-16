f11e57e7a86798250d1823f683b86fb218f0dbe8
Faten Nuraina <fatennuraina02@gmail.com>
2026-04-16 23:03:29 +0800

    done today

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
 }
@@ -12,19 +12,102 @@ class MyApp extends StatelessWidget {
     home: Scaffold(
     backgroundColor: Colors.purple,
 body: SafeArea(
-  child: Container(
-    height: 250,
-    width: 250,
-    margin: EdgeInsets.symmetric(
-      vertical: 50.0,
-          horizontal: 10,
-    ),
-    color: Colors.black,
-  child: Text('FATEN NURAINA',
-  style: TextStyle(
-  color: Colors.white
-  ),
-  ),
+  child: Column (
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Center(
+        child: CircleAvatar(
+          radius: 100,
+          backgroundImage: AssetImage('images/cat.jpg'),
+        ),
+      ),
+      SizedBox(height: 20),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite,
+            color: Colors.white,
+            size: 28,
+          ),
+          SizedBox(width: 10),
+          Text(
+            'Faten Nuraina',
+            style: GoogleFonts.lato(
+              textStyle: TextStyle(
+                fontFamily: 'Merriweather',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 40),
+      Column(
+        children: [
+          Container(
+            height: 100,
+            width: 2000,
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+                  horizontal: 10,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20)
+            ),
+          child: Center(
+            child: Text('FATEN NURAINA',
+            style: TextStyle(
+            color: Colors.black,
+
+            ),
+            ),
+          ),
+          ),
+          Container(
+            height: 100,
+            width: 2000,
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
+            ),
+            decoration: BoxDecoration(
+            color: Colors.black,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Center(
+              child: Text('FATEN NURAINA',
+                style: TextStyle(
+                    color: Colors.blue
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 2000,
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
+            ),
+            decoration: BoxDecoration(
+            color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Center(
+              child: Text('FATEN NURAINA',
+                style: TextStyle(
+                    color: Colors.black
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
   ),
 ),
     ),
