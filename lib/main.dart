366b783d241f8840d9413349036df86a0cc1f8bf
Faten Nuraina <fatennuraina02@gmail.com>
2026-04-17 21:51:42 +0800

    done today

--- lib/main.dart
+++ lib/main.dart
@@ -5,112 +5,86 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-    backgroundColor: Colors.purple,
-body: SafeArea(
-  child: Column (
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Center(
-        child: CircleAvatar(
-          radius: 100,
-          backgroundImage: AssetImage('images/cat.jpg'),
-        ),
+  Widget buildBox (String image,String name, Color bgColor, Color textColor) {
+    return Container(
+      height: 100,
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 15,
       ),
-      SizedBox(height: 20),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: EdgeInsets.symmetric(
+        horizontal: 15,
+      ),
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
         children: [
-          Icon(
-            Icons.favorite,
-            color: Colors.white,
-            size: 28,
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: AssetImage(image),
           ),
-          SizedBox(width: 10),
+          SizedBox(width: 20),
           Text(
-            'Faten Nuraina',
-            style: GoogleFonts.lato(
-              textStyle: TextStyle(
-                fontFamily: 'Merriweather',
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
-      SizedBox(height: 40),
-      Column(
-        children: [
-          Container(
-            height: 100,
-            width: 2000,
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-                  horizontal: 10,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20)
-            ),
-          child: Center(
-            child: Text('FATEN NURAINA',
+            name,
             style: TextStyle(
-            color: Colors.black,
-
-            ),
-            ),
-          ),
-          ),
-          Container(
-            height: 100,
-            width: 2000,
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 10,
-            ),
-            decoration: BoxDecoration(
-            color: Colors.black,
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Center(
-              child: Text('FATEN NURAINA',
-                style: TextStyle(
-                    color: Colors.blue
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 100,
-            width: 2000,
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 10,
-            ),
-            decoration: BoxDecoration(
-            color: Colors.white,
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Center(
-              child: Text('FATEN NURAINA',
-                style: TextStyle(
-                    color: Colors.black
-                ),
-              ),
+              color: textColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ],
       ),
-    ],
-  ),
-),
-    ),
     );
   }
-}
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/cat.jpg'),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Faten Nuraina',
+                    style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                        fontFamily: 'Merriweather',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              buildBox('images/catredhat.jpg', 'ODEN', Colors.lightBlueAccent,
+                  Colors.white),
+              buildBox('images/catdinosaur.jpg', 'OLAY', Colors.lightGreen,
+                  Colors.brown),
+              buildBox('images/catbluehat.jpg', 'LEO', Colors.deepOrange,
+                  Colors.white),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  }
